3429d537391be0400567b40ebbdb08a13fd08a1f
Rasoul Ghasemi <maths.rasoulgh455@gmail.com>
2021-04-04 16:45:32 +0430

    Deleted extra bootstrap files, Added persian locale on django admin, Solved some bugs

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -5,17 +5,21 @@ from django.utils import timezone
 # Create Models
 class Article(models.Model):
     STATUS_CHOICES = (
-        ('d', 'Draft'),
-        ('p', 'Published'),
+        ('d', 'پیش نویس'),
+        ('p', 'منتشر شده'),
     )
-    title = models.CharField(max_length=64)
-    description = models.TextField(max_length=300)
-    slug = models.SlugField(max_length=100, unique=True)
-    thumbnail = models.ImageField(upload_to='images')
-    publish = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=64, verbose_name="عنوان")
+    description = models.TextField(max_length=300, verbose_name="محتوا")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="آدرس مقاله")
+    thumbnail = models.ImageField(upload_to='images', verbose_name="تصویر مقاله")
+    publish = models.DateTimeField(default=timezone.now, verbose_name="تاریخ و زمان منتشر شدن")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name="منتشر شدن / ذخیره در پیش نویس ها")
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقاله ها'
 
     def __str__(self):
         return self.title
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,18 +1,20 @@
-#Import modules
-from django.shortcuts import render
-# from django.http import HttpResponse
+# Import modules
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Article
 
-# Create your views here.
+# Create view for blog index
 def index(request):
     context = {
         'articles': Article.objects.filter(status='p').order_by('-publish')
     }
     return render(request, 'blog/index.html', context)
 
+
+# Create view for article content
 def article_content(request, slug):
     context = {
-        'article': Article.objects.get(slug=slug)
+        'article': get_object_or_404(Article, slug=slug, status='p')
     }
     return render(request, 'blog/post.html', context)
